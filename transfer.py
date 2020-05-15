164ca1a055d206b0840f58fc20dd6bd93e94c315
WANG CHENYANG <wcy.nxu@gmail.com>
2020-05-15 12:24:47 +0800

    transfer
    transfer datasets pictures to a new folder

---
+++ transfer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding:utf8 -*-
+# @TIME  :2018/9/17 9:02
+# @Author:dazhan
+# @File  :copyfiles2dir.py
+
+import os
+import shutil
+
+source_path = os.path.abspath(r'/home/data2/DATASET/test_set_a/test_set_a/mask')
+target_path = os.path.abspath(r'/home/data2/DATASET/test_set_a/test_set_a/mask1')
+
+if not os.path.exists(target_path):
+    os.makedirs(target_path)
+
+if os.path.exists(source_path):
+    # root 所指的是当前正在遍历的这个文件夹的本身的地址
+    # dirs 是一个 list，内容是该文件夹中所有的目录的名字(不包括子目录)
+    # files 同样是 list, 内容是该文件夹中所有的文件(不包括子目录)
+    for root, dirs, files in os.walk(source_path):
+        for file in files:
+            src_file = os.path.join(root, file)
+            shutil.copy(src_file, target_path)
+            print(src_file)
+
+print('copy files finished!')
